fb416510cd393c1a0bc0f83cd94996293ccac5f5
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2023-05-08 18:28:51 +0400

    output folder name depends on source file name

--- VideoVisor/main.py
+++ VideoVisor/main.py
@@ -1,7 +1,11 @@
 from video2images import Video2Images
 
-source_video_file = "../examples/input/lada.mp4"
-output_dir = "../output"
 
-Video2Images(video_filepath=source_video_file,
-             out_dir=output_dir)
+def main():
+    source_video_file = "../examples/input/lada.mp4"
+    output_dir = "../output"
+    Video2Images(video_filepath=source_video_file, out_dir=output_dir)
+
+
+if __name__ == "__main__":
+    main()
