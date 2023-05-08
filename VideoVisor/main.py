bcdf65f49553be1abfe953dd9857357f69404937
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2023-05-08 18:16:14 +0400

    basics

---
+++ VideoVisor/main.py
@@ -0,0 +1,7 @@
+from video2images import Video2Images
+
+source_video_file = "../examples/input/lada.mp4"
+output_dir = "../output"
+
+Video2Images(video_filepath=source_video_file,
+             out_dir=output_dir)
