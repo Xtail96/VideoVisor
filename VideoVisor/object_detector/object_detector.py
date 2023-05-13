da62231b782d9810cef5d08398cf838df57fbd11
NT_WORK\Lyanguzov_AA <Lyanguzov_AA@directum.ru>
2023-05-13 20:06:05 +0400

    upd

--- VideoVisor/object_detector/object_detector.py
+++ VideoVisor/object_detector/object_detector.py
@@ -1,25 +1,8 @@
 import os.path
-
 import cv2
 import numpy as np
 from typing import List
-
-
-class BoundingBox:
-    def __init__(self, x: float, y: float, w: float, h: float):
-        self.x = x
-        self.y = y
-        self.width = w
-        self.height = h
-
-
-class DetectedObject:
-    def __init__(self, label, bbox):
-        self.label = label
-        self.bbox = BoundingBox(*bbox)
-
-    def to_string(self) -> str:
-        return f'(label: {self.label}, x:{self.bbox.x}, y:{self.bbox.y}, w:{self.bbox.width}, h:{self.bbox.height})'
+import utils
 
 
 class ObjectDetector:
@@ -34,7 +17,7 @@ class ObjectDetector:
     def get_resource_path(resource_name: str) -> str:
         return os.path.abspath(os.path.join('object_detector', 'Resources', resource_name))
 
-    def detect(self, img_path: str, target_classes: List[str] = []) -> (List[DetectedObject], str):
+    def detect(self, img_path: str, target_classes: List[str] = []) -> (List[utils.DetectedObject], str):
         print(f'Try to detect objects on {img_path}')
         image = cv2.imread(img_path)
         height, width, _ = image.shape
@@ -66,35 +49,11 @@ class ObjectDetector:
             box = boxes[box_index]
             class_index = class_indexes[box_index]
             if not target_classes or self.classes[class_index] in target_classes:
-                class_name = self.classes[class_index]
-                image = self.draw_bounding_box(image, class_name, box)
-                detected_objects.append(DetectedObject(class_name, box))
-        cv2.imwrite(img_path, image)
+                detected_objects.append(utils.DetectedObject(self.classes[class_index], box))
+
+        utils.draw_objects_on_image(detected_objects, img_path)
         print(f'{list(detected_object.to_string() for detected_object in detected_objects)} detected')
         return detected_objects, img_path
 
-    @staticmethod
-    def draw_bounding_box(image, label, box):
-        """
-        Drawing object borders with captions
-        :param image: original image
-        :param label: text fo drawing near the bbox
-        :param box: coordinates of the area around the object
-        :return: image with marked objects
-        """
-
-        x, y, w, h = box
-        start = (x, y)
-        end = (x + w, y + h)
-        color = (0, 255, 0)
-        width = 2
-        final_image = cv2.rectangle(image, start, end, color, width)
-        start = (x, y - 10)
-        font_size = 1
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        width = 2
-        text = label
-        return cv2.putText(final_image, text, start, font, font_size, color, width, cv2.LINE_AA)
-
-    def detect_all(self, images: List[str], target_classes: List[str] = []) -> List[DetectedObject]:
+    def detect_all(self, images: List[str], target_classes: List[str] = []) -> List[utils.DetectedObject]:
         return list(self.detect(img, target_classes) for img in images)
--- VideoVisor/utils.py
+++ VideoVisor/utils.py
@@ -1,11 +1,64 @@
-import os
 import shutil
 import os
+import cv2
+from typing import List
+
+
+class BoundingBox:
+    def __init__(self, x: float, y: float, w: float, h: float):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+
+    def pack(self):
+        return self.x, self.y, self.width, self.height
+
+
+class DetectedObject:
+    def __init__(self, label, bbox):
+        self.label = label
+        self.bbox = BoundingBox(*bbox)
+
+    def to_string(self) -> str:
+        return f'(label: {self.label}, x:{self.bbox.x}, y:{self.bbox.y}, w:{self.bbox.width}, h:{self.bbox.height})'
 
 
 def get_video_frames(source_video_path, output_dir):
     video_frames_dir = os.path.join(output_dir, os.path.basename(source_video_path))
-    return list(os.path.abspath(os.path.join(video_frames_dir, frame)) for frame in os.listdir(video_frames_dir))
+    frames = list(os.path.abspath(os.path.join(video_frames_dir, frame)) for frame in os.listdir(video_frames_dir))
+    frames.sort(key=len)
+    return frames
+
+
+def draw_objects_on_image(objects: List[DetectedObject], image_path: str) -> None:
+    image = cv2.imread(image_path)
+    for item in objects:
+        image = draw_bounding_box(image, item.label, item.bbox.pack())
+    cv2.imwrite(image_path, image)
+
+
+def draw_bounding_box(image, label, box):
+    """
+    Drawing object borders with captions
+    :param image: original image
+    :param label: text fo drawing near the bbox
+    :param box: coordinates of the area around the object
+    :return: image with marked objects
+    """
+
+    x, y, w, h = box
+    start = (x, y)
+    end = (x + w, y + h)
+    color = (0, 255, 0)
+    width = 2
+    final_image = cv2.rectangle(image, start, end, color, width)
+    start = (x, y - 10)
+    font_size = 1
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    width = 2
+    text = label
+    return cv2.putText(final_image, text, start, font, font_size, color, width, cv2.LINE_AA)
 
 
 def clear_folder(folder: str):
